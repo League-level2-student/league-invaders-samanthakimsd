e9d4a74a550590256d4e98f23324cdffd9bc61c1
root <root@c5b3616de711>
2020-05-25 00:26:17 +0000

    i did league invaders.

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,17 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+public static final int WIDTH = 500;
+public static final int HEIGHT = 800;
+JFrame frame;
+GamePanel gp;
+
+void LeaugeInvaders(){
+	frame = new JFrame();
+	gp = new GamePanel();
+}
+public static void main(String[]args) {
+	
+}
 
 }
